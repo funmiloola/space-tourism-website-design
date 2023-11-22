ba91ceb8b58bc5fcf5ef35736474f5234d587e1a
unknown <funmilolaadebiyi52@gmail.com>
2023-11-22 09:35:26 +0100

    home-section(nav-bar-changes)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
-   <home-nav-section/>
+    <home-nav-section />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomeNavSection from '@/components/Home-Nav-section.vue';
+import HomeNavSection from "@/components/Home-Nav-section.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HomeNavSection
+    HomeNavSection,
+  },
+};
+</script>
+<style scoped>
+*{
+ margin:0;
+}
+.home{
+    background-image: url(@/assets/background-home-desktop.jpg);
+  height: 100vh;
+  width: 100%;
+  margin: 0;
+}
+@media (min-width: 600px) and (max-width: 1024px) {
+  .home {
+    background-image: url(@/assets/background-home-tablet.jpg);
   }
 }
-</script>
+@media (max-width: 599px) {
+  .home {
+    background-image: url(@/assets/background-home-mobile.jpg);
+  }
+}
+
+</style>
